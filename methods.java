77c9e39eaa1189fbf304c0f62b3a7e73f1b9971e
chinmairaj <chinmait04@gmail.com>
2025-05-06 20:58:29 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,29 @@
+public class methods {
+    public static void func(String s,int n) {
+        for(int i = 0; i<n; i++){
+            System.out.println(s);
+        } 
+    }
+    public static void main(String[]args){
+        func("chinmai", 3);
+        func("yaswanth", 4);
+   }
+          
+
+
+        /*String s = "chinmai";
+        int n =3;
+        for(int i = 0; i<n; i++){
+            System.out.println(s);
+        }
+        String name = "yaswanth";
+        int num =3;
+        for(int i = 0; i<num; i++){
+            System.out.println(name);
+        }
+    }*/
+    
+}
+
+    
+ 
