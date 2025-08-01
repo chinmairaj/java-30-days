12233dd0a2b0ca018d00280efb2b0667027c525b
chinmairaj <chinmait04@gmail.com>
2025-08-01 21:26:02 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,53 @@
+public class forloop {
+    public static void main(String[] args) {
+        /*for(int i = 0; i <= 50; i++){
+            System.out.println(i +"HEY CHINMAYEE");
+            
+        }*/
+       /* int i = 1;
+            for(;i<=5;){
+                System.out.println("hey chinmayee");
+                i++;
+            }
+            int i =1, j= 10;
+            for(i = 1,j=10; i <= j;System.out.println(i), i++,j--);//loop body is option
+                System.out.println(i);*/
+               /*  int n = 10,  sum = 0;
+              for(int i = 1; i<=n;i++){
+                sum += i;
+              }
+                
+                System.out.println("sum:" + sum);//sum of 1st n natural number*/
+                /*int n = 7, fact = 1;
+                for(int i = 1;i<=n;i++){
+                  fact *=i; 
+                System.out.println("factorial :"+ fact);*/// print factorial of a number using for loop
+                int number = 7;
+                for(int i = 1;i<=10;i++){
+                  System.out.println(number + "*" + i + "=" + (number * i));// mutiplication of a number
+                }
+int n = 20;
+for(int i = 2; i<= n;i+=2){
+  System.out.println(i);// this is how you find an even number
+
+}
+int num = 30;
+boolean isPrime = true;
+for(int i = 2; i<=num/2; i++){
+  if(num % i == 0){
+    isPrime = false;
+    break;
+  }
+  
+}
+System.out.println( num +(isPrime ? "is prime" : "is not prime"));
+
+
+            
+              
+              
+
+
+    }
+    
+}
