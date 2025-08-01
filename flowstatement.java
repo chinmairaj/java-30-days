6d45baea36182875d89996f3ee7bc9b1e4dc82fd
chinmairaj <chinmait04@gmail.com>
2025-08-01 21:25:09 +0530

    Add files via upload

---
+++ flowstatement.java
@@ -0,0 +1,28 @@
+ public class flowstatement{
+    public static void main(String[] args) {
+        /*  String chinmai = "girl" ;
+        if(chinmai.equals ("girl")){
+         System.out.println("right");
+         }
+        else{
+                System.out.println("left");
+            }
+            
+             if (1 + 2 == 3) {
+                System.out.println("given condition is true");
+            }
+            else{
+                System.out.println("given condition is not true");
+            }
+
+            }  */    
+              int temperature = 45;
+              if (temperature>=40) {
+                System.out.println("heatwave alert ! stay indoors!!");
+              }
+              else{
+                System.out.println("you can go out");
+              }
+    } 
+} 
+
