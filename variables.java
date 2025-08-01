a8b8814641b88352e4a73f482a7e5f98abc2aa2f
chinmairaj <chinmait04@gmail.com>
2025-08-01 21:30:17 +0530

    Add files via upload

--- variables.java
+++ variables.java
@@ -1,7 +1,19 @@
 public class variables {
+    int studentage=21;
+    int student1age=20;
+    int student2age=student1age-1;
+    void display(){
+        System.out.println("roll no is" + studentage);
+        System.out.println("roll no is" + student1age);
+        System.out.println("roll no is" + student2age);
+    }
     public static void main(String[] args) {
-        String clever = "chinmai is clever" ;
-        System.out.println(clever);
+        variables obj = new variables();
+        System.out.println(obj.studentage);
+        System.out.println(obj.student1age);
+        obj .display();
+       /*  String clever = "chinmai is clever" ;
+        System.out.println(clever);*/
 
     }
     
