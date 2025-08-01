5e2f6902713a1766eb339fa7e9db5c286c0e4678
chinmairaj <chinmait04@gmail.com>
2025-08-01 21:33:52 +0530

    Add files via upload

---
+++ escape.java
@@ -0,0 +1,13 @@
+public class escape {
+    public static void main(String[] args){
+        String s = "i \nam \r chinmayee";//\r for removing before word
+        //System.out.println("i \nam \nchinmayee");
+        System.out.println(s);
+        System.out.println("name:\tchinmayee\ncgpa:\t8.0");// \n for new line;\t for new tab
+        System.out.println("\"chinmayee\"");// directly we cant print double qoutes("")directly\"/"
+        System.out.println("hey \fchinmayee");
+                8
+
+    }
+    
+}
